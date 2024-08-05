cecf6e7b779a2ef18111914aa2a59d3bbb63d506
Asif <asifhaque0505@gmail.com>
2024-08-05 11:21:14 +0530

    modal-dir

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,8 @@ import { Route, Routes } from "react-router-dom";
 import Photos from "./pages/Photos";
 import { lazy, Suspense, useState } from "react";
 import Loading from "./components/loaders/Loading";
-// const Videos = lazy(() => import("./pages/Videos"));
-// const Modal = lazy(() => import("./components/modal/Modal"));
-import Videos from "./pages/Videos";
-import Modal from "./components/modal/Modal";
+const Videos = lazy(() => import("./pages/Videos"));
+const Modal = lazy(() => import("./components/modal-dir/Modal"));
 
 function App() {
   const [scroll, setScroll] = useState(false);
